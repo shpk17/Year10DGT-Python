35cbb338d5c0f06b2c9a787c0a1886bb14d50954
shpk17 <168596260+shpk17@users.noreply.github.com>
2024-05-08 10:05:04 +1200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+#functions go here
+def intro():
+    print("Welcome")
+    print()
+    print("Instructions go here")
+    return "" #I am returning (displaying/using) all items
+                #in the code
+(function) def
+
+# Main routine
+intro() # Thi scalls the function so that we can use it.
